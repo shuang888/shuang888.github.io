ab828e1521d69f9b0ba4e5b618abfa4f5509d4b5
shuang888 <49227487+shuang888@users.noreply.github.com>
2024-03-13 18:58:38 +0700

    Update CN影院[飞].js

--- js/CN影院[飞].js
+++ js/CN影院[飞].js
@@ -5,7 +5,7 @@ var rule = {
     模板:'cnys',
     host:'https://cnys.tv',
     // url:'/vodshow/fyclass--------fypage---.html',
-    url:'/vodshow/fyfilter.html',
+    url:'/vodshow-fyfilter.html',
     filterable:1,//是否启用分类筛选,
     filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
     filter: {
